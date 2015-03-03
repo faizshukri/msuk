a52e323c9541b606886a70371188f065fe831614
Faiz Shukri <faizshukri90@gmail.com>
2015-03-03 19:45:40 +0000

    Add main scss
    Signed-off-by: Faiz Shukri <faizshukri90@gmail.com>

--- gulpfile.js
+++ gulpfile.js
@@ -14,12 +14,15 @@ var elixir = require('laravel-elixir');
 elixir(function(mix) {
 
     // Before login
+    mix.sass("home.scss");
+
     mix.scripts([
         'assets/jquery/dist/jquery.js',
         'assets/bootstrap/dist/js/bootstrap.js'
     ], 'public/js/guest.js','public');
 
     mix.styles([
+        'css/home.css',
         'assets/normalize-css/normalize.css',
         'assets/bootstrap/dist/css/bootstrap.css'
     ], 'public/css/guest.css','public');
@@ -42,8 +45,10 @@ elixir(function(mix) {
 
     mix.version(['css/guest.css', 'js/guest.js', 'css/app.css', 'js/app.js']);
 
-    mix.copy('public/css/guest.css.map', 'public/build/css/guest.css.map')
-        .copy('public/assets/mapbox.js/images', 'public/build/css/images')
-        .copy('public/css/app.css.map', 'public/build/css/app.css.map');
+    mix.copy('public/assets/mapbox.js/images', 'public/build/css/images')
+        .copy('public/css/guest.css.map', 'public/build/css/guest.css.map')
+        .copy('public/css/app.css.map', 'public/build/css/app.css.map')
+        .copy('public/js/guest.js.map', 'public/build/js/guest.js.map')
+        .copy('public/js/app.css.map', 'public/build/js/app.js.map');
 
 });
